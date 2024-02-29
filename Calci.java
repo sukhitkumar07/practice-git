1efe46ccc326208ccbdc49001ad520e92233793c
sukhitkumar07 <sukhitkumardbg05@gmail.com>
2024-02-29 12:17:15 +0530

    Implementation provided for Calci

--- Calci.java
+++ Calci.java
@@ -1,3 +1,9 @@
 interface Calci{
-public static int getSum(int n1, int n2);
+public  int getSum(int n1, int n2);
+}
+
+class CalciImpl implements Calci{
+    public int getSum(int n1, int n2){
+        return n1+n2;
+    }
 }
